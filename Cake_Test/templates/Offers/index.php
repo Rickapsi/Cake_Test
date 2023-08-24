e0c866bbed0ae1715c43fd9cd8cee7526e5864b1
Matthew Rickatson <Matthew.Rickatson@outlook.com>
2023-08-24 14:00:33 +0800

    Added Jquery and Jquery auto-complete with styling

--- Cake_Test/templates/Offers/index.php
+++ Cake_Test/templates/Offers/index.php
@@ -1,4 +1,30 @@
-<!-- In src/Template/Offers/index.ctp -->
+
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<link rel="stylesheet" href="https://cdn.datatables.net/1.11.5/css/jquery.dataTables.min.css">
+<script src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.min.js"></script>
+<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+
+<style>
+    /* Custom styles for the autocomplete suggestions */
+    .ui-autocomplete {
+        max-height: 200px;
+        max-width: 500px;
+        overflow-y: auto;
+        border: 1px solid #ccc;
+        background-color: white;
+    }
+
+    .ui-autocomplete .ui-menu-item {
+        padding: 5px;
+        cursor: pointer;
+    }
+
+    .ui-autocomplete .ui-menu-item:hover {
+        background-color: #f0f0f0;
+    }
+</style>
+
+
 <table id="offersTable" class="table">
     <thead>
         <tr>
@@ -25,3 +51,35 @@
         <?php endforeach; ?>
     </tbody>
 </table>
+
+<script>
+    $(document).ready(function() {
+        $('#offersTable').DataTable();
+
+        $.fn.dataTable.ext.search.push(
+            function(settings, searchData, index, rowData, counter) {
+                var searchTerm = $('#offersTable_filter input[type="search"]').val().toLowerCase();
+                var nameData = rowData[0].toLowerCase(); // Assuming "Name" column is the first column (index 0)
+
+                if (nameData.includes(searchTerm)) {
+                    return true;
+                }
+                return false;
+            }
+        );
+
+        // Initialize Autocomplete for the modified search input
+        $('#offersTable_filter input[type="search"]').autocomplete({
+            source: [
+                <?php foreach ($offers as $offer): ?>
+                    "<?= h($offer->name) ?>",
+                <?php endforeach; ?>
+            ],
+            select: function(event, ui) {
+                $('#offersTable_filter input[type="search"]').val(ui.item.value).trigger('input');
+            }
+        });
+    });
+
+    
+</script>
