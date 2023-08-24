380f2221100fca87e7618ada073456c412cda044
Matthew Rickatson <Matthew.Rickatson@outlook.com>
2023-08-24 13:30:53 +0800

    Changes to the home.php

--- Cake_Test/templates/Pages/home.php
+++ Cake_Test/templates/Pages/home.php
@@ -54,54 +54,14 @@ endif;
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cake Test</title>
-    
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
-    
     <!-- jQuery -->
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    
     <!-- DataTables JavaScript -->
     <script src="https://cdn.datatables.net/1.11.7/js/jquery.dataTables.min.js"></script>
 </head>
 <body>
-    <table id="offersTable" class="table">
-    <thead>
-        <tr>
-            <th>Name</th>
-            <th>Requirements</th>
-            <th>Description</th>
-            <th>ECPC</th>
-            <th>Click URL</th>
-        </tr>
-    </thead>
-    <tbody>
-        <!-- Data will be dynamically inserted here -->
-    </tbody>
-</table>
-
-<script>
-    $(document).ready(function() {
-        $('#offersTable').DataTable({
-            ajax: {
-                url: 'URL_TO_YOUR_API_ENDPOINT', // Replace with your actual API endpoint
-                dataSrc: 'data' // Assuming the API response has a 'data' property
-            },
-            columns: [
-                { data: 'name' },
-                { data: 'requirements' },
-                { data: 'offer_desc' },
-                { data: 'ecpc' },
-                {
-                    data: 'click_url',
-                    render: function(data, type, row) {
-                        return '<a href="' + data + '" target="_blank">' + data + '</a>';
-                    }
-                }
-            ]
-        });
-    });
-</script>
-
 </body>
+</html>
 
