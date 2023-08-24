11407cec28bf062b88a7a15b628d696490dedfd1
Matthew Rickatson <Matthew.Rickatson@outlook.com>
2023-08-24 09:48:21 +0800

    table creation script

---
+++ Cake_Test/config/schema/create_tables.sql
@@ -0,0 +1,9 @@
+
+CREATE TABLE entries (
+    id INT PRIMARY KEY,
+    name VARCHAR(50),
+    anchor VARCHAR(50),
+    requirements VARCHAR(250),
+    description VARCHAR(250),
+    ecpc VARCHAR(20)
+);
